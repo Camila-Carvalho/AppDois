f4e378a21c551e1beff7041e913d85fb241c9f7d
“Camila <camilaibrsistemas@gmail.com>
2020-08-04 21:53:50 -0300

    Ajustado a formatação da moeda para o padrão Brasileiro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
 import { RouterModule, Routes } from '@angular/router';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import {registerLocaleData} from '@angular/common';
+import localePT from '@angular/common/locales/pt';
+
+registerLocaleData(localePT);
 
 import { ROUTES } from './app.routes';
 
@@ -34,7 +38,7 @@ import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
     HttpClientModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
